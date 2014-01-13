4da0dc3f2815f74018fe077fb5d96e265175b00d
James Hunter <james.hunter@me.com>
2014-01-13 15:41:34 +0000

    Added the beginning of a menu. ALso a couple of smaller changes in places, but nothing significant.

--- directory-challenges.rb
+++ directory-challenges.rb
@@ -3,30 +3,46 @@
 require 'date'
 
 def print_header
-	print "-".center(50, '-') + "\n"
-	print "  The students of Makers Academy  ".center(50, '-') + "\n"
-	print "-".center(50, '-') + "\n"
-	print "-".center(50, '-') + "\n"
+	print "".center(93, '=') + "\n"
+	print "  THE STUDENTS OF MAKERS ACADEMY  ".center(93, '=') + "\n"
+	print "".center(93, '=') + "\n"
 end
 
-def print_names(students)
+def print_names(students, filtered_by_month = false, month = :January)
 	count = 0
-	while count < students.length
-		if students[count][:name].length < 12 # Also, if students[count][:name][0].capitalise == 'A'
-			puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)".center(50)
-			puts "They enjoy #{students[count][:hobbies]}.".center(50) unless students[count][:hobbies] == ""
-			puts "They were born in #{students[count][:country]}.".center(50) unless students[count][:country] == ""
-			puts "They are #{students[count][:height]} tall.".center(50) unless students[count][:height] == ""
-			puts "-".center(50, '-')
+	unless students.empty?
+		if filtered_by_month
+			puts "  #{month} Cohort  ".center(93, '-')
+			puts "".center(93, '-')
 		end
-		count += 1
+		print "   " + "Name".ljust(15)
+		filtered_by_month ? print(" ".ljust(5)) : print("Cohort".ljust(25))
+		print "Hobbies".ljust(20) + "Place of Birth".ljust(20) + "Height".ljust(10) + "\n"
+		
+		puts "-".center(93, '-')
+		while count < students.length
+			if students[count][:name].length < 12 # Also, if students[count][:name][0].capitalise == 'A'
+				print "#{count+1}. " + "#{students[count][:name]}".ljust(15)
+				#print "#{students[count][:cohort]} cohort".ljust(25) unless filtered_by_month
+				filtered_by_month ? print(" ".ljust(5)) : print("#{students[count][:cohort]} cohort".ljust(25))
+				print "#{students[count][:hobbies]}".ljust(20)
+				print "#{students[count][:country]}".ljust(20)
+				puts "#{students[count][:height]}".ljust(10)
+				#puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)".center(50)
+				#puts "They enjoy #{students[count][:hobbies]}.".center(50) unless students[count][:hobbies] == ""
+				#puts "They were born in #{students[count][:country]}.".center(50) unless students[count][:country] == ""
+				#puts "They are #{students[count][:height]} tall.".center(50) unless students[count][:height] == ""
+				puts "".center(93, '-')
+			end
+			count += 1
+		end
+		# puts "-".center(50, '-')
 	end
-	puts "-".center(50, '-') unless students.empty?
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great student#{names.length == 1 ? "" : "s"}".center(50)
-	puts "-".center(50, '-')
+	puts "Overall, we have #{names.length} great student#{names.length == 1 ? "" : "s"}".center(93)
+	puts "-".center(93, '-')
 end
 
 def input_students(cohort_list)
@@ -78,7 +94,7 @@ end
 def print_by_cohort(students)
 	Date::MONTHNAMES.each do |month|
 		if !month.nil?
-			print_names students.select{|entry| entry[:cohort] == month.intern}
+			print_names(students.select{|entry| entry[:cohort] == month.intern}, true, month)
 		end
 	end 
 end
@@ -93,9 +109,33 @@ def print_everything(students, cohort_list)
 	unless students.empty?
 		print_header
 		print_by_cohort(students)
+		# print_names(students)
 		print_footer(students)
 	else
 		puts "No students in directory, sorry."
+	end
+end
+
+def interactive_menu(cohort_list)
+	students = []
+	loop do
+		# 1. Print the menu and as the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		# 2. Read the input and save it into a variable
+		selection = gets.chomp
+		# 3. Do what the user has asked
+		case selection
+		when "1"
+			students = input_students(cohort_list)
+		when "2"
+			print_everything(students, cohort_list)
+		when "9"
+			exit
+		else
+			puts "I don't know what you meant, try again"
+		end
 	end
 end
 
@@ -119,6 +159,7 @@ cohort_list = {
 }
 
 # nothing happens until we call the methods
-students = input_students(cohort_list)
+#students = input_students(cohort_list)
+#print_everything(students, cohort_list)
 
-print_everything(students, cohort_list)
+interactive_menu(cohort_list)
